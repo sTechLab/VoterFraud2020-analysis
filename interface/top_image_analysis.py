a8e018e1b8ee11004288e3b19fe9948f9ef3aeea
Anton Abilov <anton.abilov@gmail.com>
2021-01-19 17:16:52 +0100

    Implemented filtering in Top Images page

--- interface/top_image_analysis.py
+++ interface/top_image_analysis.py
@@ -4,9 +4,22 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
+CLUSTER_PROMOTERS = "Promoters of Voter Fraud Claims"
+CLUSTER_DETRACTORS = "Detractors of Voter Fraud Claims"
+CLUSTER_SUSPENDED = "Suspended Users"
+
+def get_images_df(cluster):
+    if (cluster == CLUSTER_PROMOTERS):
+        df = pd.read_csv("./interface/data/top_10_retweeted_promoters.csv", delimiter=";").drop("Unnamed: 0", axis=1)
+        return df[["num_of_unique_tweet_id", "sum_of_retweets_by_cluster_1_to_4", "sum_of_retweet_count", "image_url"]]
+    elif (cluster == CLUSTER_DETRACTORS):
+        df = pd.read_csv("./interface/data/top_10_retweeted_detractors.csv", delimiter=";").drop("Unnamed: 0", axis=1)
+        return df[["num_of_unique_tweet_id", "retweets_by_cluster_0", "sum_of_retweet_count", "image_url"]]
+    elif (cluster == CLUSTER_SUSPENDED):
+        df = pd.read_csv("./interface/data/top_10_retweeted_suspended.csv", delimiter=";").drop("Unnamed: 0", axis=1)
+        return df[["num_of_unique_tweet_id", "retweets_by_suspended", "sum_of_retweet_count", "image_url"]]
 
 def get_top_image_analysis_page(shared_state):
-    coverage_stats = shared_state.coverage_stats
 
     st.header("Top Images")
     st.markdown(
@@ -18,19 +31,15 @@ def get_top_image_analysis_page(shared_state):
         
         We present the same analysis for users from the suspended set, i.e. the most retweeted images by users that were suspended from Twitter.
 
-    """.format(
-            coverage_stats["recent_tweet_count"] - coverage_stats["quote_tweet_count"],
-            coverage_stats["quote_tweet_count"],
-            coverage_stats["retweet_count"],
-        )
+    """
     )
 
     selected_cluster = st.selectbox(
         "Select cluster",
         [
-            "Promoters of Voter Fraud Claims",
-            "Detractors of Voter Fraud Claims",
-            "Suspended Users",
+            CLUSTER_PROMOTERS,
+            CLUSTER_DETRACTORS,
+            CLUSTER_SUSPENDED,
         ],
     )
 
@@ -45,26 +54,20 @@ def get_top_image_analysis_page(shared_state):
         unsafe_allow_html=True,
     )
 
-    st.markdown(
-        """
-        ### 1) 4,700 retweets from detractors of voter fraud claims
-        - Appeared in 20 tweets.  
-        - Total retweet count: 10,743.  
+    df = get_images_df(selected_cluster)
 
-        ![Image 1](https://storage.googleapis.com/vote-twitter-stream/5547006013472768.jpg)
-    """
-    )
+    for i, tweet_count, cluster_retweet_count, total_retweet_count, image_url in df.itertuples():
 
-    st.markdown(
-        """
-        ### 2) 3,424 retweets from detractors of voter fraud claims
-        - Appeared in 8 tweets.  
-        - Total retweet count: 6,425.  
+        rank = i + 1
+        st.markdown(
+            """
+            ### {}) {:,} retweets from {}
+            - Appeared in {} tweets.  
+            - Total retweet count: {:,}.  
 
-        ![Image 2](https://storage.googleapis.com/vote-twitter-stream/4674182692470784.jpg)
-    """
-    )
-
+            ![Image {}]({})
+        """.format(rank, cluster_retweet_count, selected_cluster, tweet_count, total_retweet_count, rank, image_url)
+        )
 
 if __name__ == "__main__":
     get_top_image_analysis_page()
